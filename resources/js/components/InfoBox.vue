36dd3495f47054034c365d59a750f2f36497ff35
Faishal <ishal.2401@gmail.com>
2026-04-13 14:55:44 +0700

    Add slot component

---
+++ resources/js/components/InfoBox.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Info } from 'lucide-vue-next';
+
+defineProps<{
+  class?: string;
+}>();
+</script>
+
+<template>
+  <div class="rounded-lg border border-blue-600 bg-blue-600/10 px-4 py-3" :class="class">
+    <div class="flex items-center gap-x-2 text-blue-400 mb-2">
+      <Info class="size-4" />
+      <strong>This is a info message</strong>
+    </div>
+
+    <div class="text-sm text-muted-foreground"><slot /></div>
+  </div>
+</template>
--- resources/js/components/chat/List.vue
+++ resources/js/components/chat/List.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import InfoBox from '../InfoBox.vue';
 import Bubble from './Bubble.vue';
 
 defineProps<{
@@ -8,6 +9,13 @@ defineProps<{
 
 <template>
   <div class="flex-1 space-y-1 overflow-y-auto p-4">
+    <InfoBox v-if="selectedContact.id === 2" class="mb-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+      itaque architecto, quidem temporibus placeat eaque repudiandae recusandae
+      dolorem modi beatae, vitae nesciunt fugiat doloremque. Nihil, recusandae
+      corporis.
+    </InfoBox>
+
     <Bubble
       v-for="message in selectedContact.messages"
       :key="message.id"
