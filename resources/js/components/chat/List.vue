72b65607a438c9dbf532f31a376d6ccde9889a82
Faishal <ishal.2401@gmail.com>
2026-04-14 15:08:07 +0700

    Add auto-scroll with useTemplateRef in chat list

--- resources/js/components/chat/List.vue
+++ resources/js/components/chat/List.vue
@@ -1,21 +1,32 @@
 <script setup lang="ts">
 import { selectedContactMessagesKey } from '@/types/keys';
-import { inject } from 'vue';
+import { inject, nextTick, useTemplateRef, watch } from 'vue';
 import InfoBox from '../InfoBox.vue';
 import Bubble from './Bubble.vue';
 
-defineProps<{
+const props = defineProps<{
   contactId: number;
 }>();
 
 const messages = inject(selectedContactMessagesKey);
+const listRef = useTemplateRef('list');
+
+const scrollToBottom = () => {
+  nextTick(() => {
+    if (listRef.value) {
+      listRef.value.scrollTop = listRef.value.scrollHeight;
+    }
+  });
+};
+
+watch([() => messages?.value?.length, () => props.contactId], scrollToBottom, { immediate: true });
 </script>
 
 <template>
-  <div class="flex-1 space-y-1 overflow-y-auto p-4">
+  <div ref="list" class="flex-1 space-y-1 overflow-y-auto p-4">
     <TransitionGroup name="message">
       <!-- Demo Info Box -->
-      <InfoBox v-if="contactId === 2" class="mb-4" key="info-box">
+      <InfoBox v-if="props.contactId === 2" class="mb-4" key="info-box">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
         itaque architecto, quidem temporibus placeat eaque repudiandae
         recusandae dolorem modi beatae, vitae nesciunt fugiat doloremque. Nihil,
